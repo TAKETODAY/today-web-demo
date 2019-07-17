cd927cec7cb843fa6d0387f4b8dce664e8b192fd
TAKETODAY <2228592280@qq.com>
2019-07-17 23:32:59 +0800

    add ApplicationExceptionAdvice

---
+++ src/main/java/cn/taketoday/web/demo/config/ApplicationExceptionAdvice.java
@@ -0,0 +1,81 @@
+/**
+ * Original Author -> 杨海健 (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2019 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *   
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+package cn.taketoday.web.demo.config;
+
+import javax.servlet.http.HttpServletResponse;
+
+import cn.taketoday.web.annotation.ControllerAdvice;
+import cn.taketoday.web.annotation.ExceptionHandler;
+import cn.taketoday.web.demo.view.Json;
+import cn.taketoday.web.exception.AccessForbiddenException;
+import cn.taketoday.web.exception.FileSizeExceededException;
+import cn.taketoday.web.exception.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author TODAY <br>
+ *         2019-04-20 16:22
+ */
+@Slf4j
+@ControllerAdvice
+public class ApplicationExceptionAdvice {
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public Json unauthorized() {
+        log.error("Unauthorized");
+        return Json.unauthorized();
+    }
+
+    @ExceptionHandler(AccessForbiddenException.class)
+    public Json accessForbidden() {
+        log.error("Access Forbidden");
+        return Json.badRequest("权限不足");
+    }
+
+    @ExceptionHandler({ IllegalArgumentException.class })
+    public Json badRequest(IllegalArgumentException illegalArgumentException) {
+        log.error("Bad request", illegalArgumentException);
+        return Json.badRequest(illegalArgumentException.getMessage());
+    }
+
+    @ExceptionHandler({ NotFoundException.class })
+    public Json notFound(NotFoundException exceededException) {
+        log.error(exceededException.getMessage(), exceededException);
+
+        return Json.notFound();
+    }
+
+    @ExceptionHandler({ FileSizeExceededException.class })
+    public Json badRequest(FileSizeExceededException exceededException) {
+        log.error(exceededException.getMessage(), exceededException);
+
+        return Json.badRequest("上传文件大小超出限制");
+    }
+
+    @ExceptionHandler
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Json error(Exception exception, HttpServletResponse response) {
+
+        log.error("An Exception occurred", exception);
+
+        return Json.failed(exception.getMessage());
+    }
+
+}
---
+++ src/main/java/cn/taketoday/web/demo/config/RedisConfiguration.java
@@ -0,0 +1,82 @@
+/**
+ * Original Author -> 杨海健 (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2019 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *   
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+package cn.taketoday.web.demo.config;
+
+import cn.taketoday.context.annotation.Props;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author TODAY <br>
+ *         2019-02-28 19:01
+ */
+@Getter
+@Setter
+//@Configuration
+@Props(prefix = { "redis.pool.", "redis." })
+public class RedisConfiguration {
+
+    private int maxIdle;
+    private int minIdle;
+    private int timeout;
+    private int maxTotal;
+
+    private int database;
+    private String address;
+
+    private String password;
+    private String clientName;
+
+    private int connectTimeout;
+
+    /**
+     * 
+     * @param redisson
+     * @return
+     */
+//	@Singleton("limitLock")
+//	public Lock limitLock(Redisson redisson) {
+//		return redisson.getLock("limitLock");
+//	}
+
+//	@Singleton
+//	public Codec codec() {
+//		return new FstCodec();
+//	}
+
+//	@Singleton(destroyMethods = "shutdown")
+//	public Redisson redisson(Codec codec) {
+//		Config config = new Config();
+//		config.setCodec(codec)//
+//				.useSingleServer()//
+//				.setAddress(address)//
+//				.setTimeout(timeout)//
+//				.setPassword(password)//
+//				.setDatabase(database)//
+//				.setClientName(clientName)//
+//				.setConnectionPoolSize(maxTotal)//
+//				.setConnectTimeout(connectTimeout)//
+//				.setConnectionMinimumIdleSize(minIdle)//
+//				.setConnectionMinimumIdleSize(maxIdle);
+//
+//		return (Redisson) Redisson.create(config);
+//	}
+
+}
